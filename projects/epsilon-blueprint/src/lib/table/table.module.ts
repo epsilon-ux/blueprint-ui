e3b6eb2beb2694d12bbac803fcd3779fec8b7654
Ash Johns <ashleypjohns@gmail.com>
2020-01-24 13:59:12 -0500

    point table earch at the search component

--- projects/epsilon-blueprint/src/lib/table/table.module.ts
+++ projects/epsilon-blueprint/src/lib/table/table.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { SearchModule } from '../search/search.module';
 import { TableComponent } from './table.component';
 import { TableBodyComponent } from './table-body/table-body.component';
 import { PaginationComponent } from './table-footer/pagination/pagination.component';
 import { TableFooterComponent } from './table-footer/table-footer.component';
-import { SearchComponent } from './table-filters/search/search.component';
 import { ColumnSelectorComponent } from './table-footer/column-selector/column-selector.component';
 import { DisplayDensityComponent } from './table-footer/display-density/display-density.component';
 import { ActionsComponent } from './table-body/table-columns/actions/actions.component';
@@ -18,12 +18,11 @@ import { ActionsComponent } from './table-body/table-columns/actions/actions.com
     TableBodyComponent,
     PaginationComponent,
     TableFooterComponent,
-    SearchComponent,
     DisplayDensityComponent,
     ColumnSelectorComponent,
     ActionsComponent
   ],
-  imports: [CommonModule, FormsModule, NgSelectModule, RouterModule.forChild([])],
+  imports: [CommonModule, FormsModule, NgSelectModule, SearchModule, RouterModule.forChild([])],
   exports: [TableComponent],
   providers: [],
   bootstrap: [TableComponent]
