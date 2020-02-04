46da75292d696f9909d74bf866dc3cf313162b1a
Kasey Labelle <kasey.labelle@epsilon.com>
2020-02-04 18:14:30 -0500

    revert changes to model

--- src/app/modules/component-library/library/table/models/column.ts
+++ src/app/modules/component-library/library/table/models/column.ts
@@ -1,17 +1,16 @@
 export class Column {
-  key: string;
-  headerText: string;
-  isColumnDisplayed: boolean;
-  type?: ColumnType;
-}
-
-export enum ColumnType {
-  ID,
-  TEXT,
-  LINK,
-  DATE,
-  BOOLEAN,
-  INPUT,
-  STATUS,
-  DROPDOWN
-}
+    key: string;
+    headerText: string;
+    isColumnDisplayed: boolean;
+    type?: ColumnType;
+  }
+  export enum ColumnType {
+    ID,
+    TEXT,
+    LINK,
+    DATE,
+    BOOLEAN,
+    INPUT,
+    STATUS,
+    DROPDOWN
+  }
