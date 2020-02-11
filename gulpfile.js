f51206ff7bb33662b041f71e41cdb8e74ad17214
Ash Johns <ashleypjohns@gmail.com>
2020-02-11 16:11:31 -0500

    update deploy script

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,27 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 
-exports.copyScss = () => {
+const copyStylesFolder = cb => {
+  return src('projects/epsilon-blueprint/src/styles/*.scss')
+    .pipe(dest('dist/epsilon-blueprint/styles/'));
+}
+
+const copyStyles = cb => {
+  return src('projects/epsilon-blueprint/src/styles.scss')
+    .pipe(dest('dist/epsilon-blueprint/'));
+}
+
+exports.copyScss = parallel(copyStylesFolder, copyStyles);
+
+exports.watchScss = () => {
   watch(
     'projects/epsilon-blueprint/src/styles/*.scss',
     { ignoreInitial: false },
-    cb => {
-      return src('projects/epsilon-blueprint/src/styles/*.scss')
-        .pipe(dest('dist/epsilon-blueprint/styles/'));
-    }
+    cb => copyStylesFolder(cb)
   );
   
   watch(
     'projects/epsilon-blueprint/src/styles.scss',
     { ignoreInitial: false },
-    cb => {
-      return src('projects/epsilon-blueprint/src/styles.scss')
-        .pipe(dest('dist/epsilon-blueprint/'));
-    }
+    cb => copyStyles(cb)
   );
 };
