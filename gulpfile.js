73d5add544c8ef892bb278f0adcfaa658441981c
Ash Johns <ashleypjohns@gmail.com>
2020-02-11 14:44:14 -0500

    improve gulpfile spacing

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 
 exports.copyScss = () => {
-  watch('projects/epsilon-blueprint/src/styles/*.scss', {ignoreInitial: false}, cb => {
-    return src('projects/epsilon-blueprint/src/styles/*.scss')
-      .pipe(dest('dist/epsilon-blueprint/styles/'));
-  });
+  watch(
+    'projects/epsilon-blueprint/src/styles/*.scss',
+    { ignoreInitial: false },
+    cb => {
+      return src('projects/epsilon-blueprint/src/styles/*.scss')
+        .pipe(dest('dist/epsilon-blueprint/styles/'));
+    }
+  );
   
-  watch('projects/epsilon-blueprint/src/styles.scss', {ignoreInitial: false}, cb => {
-    return src('projects/epsilon-blueprint/src/styles.scss')
-      .pipe(dest('dist/epsilon-blueprint/'));
-  });
+  watch(
+    'projects/epsilon-blueprint/src/styles.scss',
+    { ignoreInitial: false },
+    cb => {
+      return src('projects/epsilon-blueprint/src/styles.scss')
+        .pipe(dest('dist/epsilon-blueprint/'));
+    }
+  );
 };
