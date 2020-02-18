d650f017658baa87787b060861646d521de9d75a
Ash Johns <ashleypjohns@gmail.com>
2020-02-18 11:43:25 -0500

    create action component

---
+++ projects/epsilon-blueprint/src/lib/action/action.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-action',
+  templateUrl: './action.component.html',
+  styleUrls: ['./action.component.scss']
+})
+export class ActionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
