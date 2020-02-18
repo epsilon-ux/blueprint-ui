47eebb150131a1c5213e22209f4b94cc8175609a
Ash Johns <ashleypjohns@gmail.com>
2020-02-18 15:45:08 -0500

    add action component basics

--- projects/epsilon-blueprint/src/lib/action/action.component.ts
+++ projects/epsilon-blueprint/src/lib/action/action.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'lib-action',
+  selector: 'bp-action',
   templateUrl: './action.component.html',
   styleUrls: ['./action.component.scss']
 })
 export class ActionComponent implements OnInit {
 
+  @Input() text: string;
+  @Input() element: string = 'button'; // button, a
+  @Input() classes: string = 'btn-primary';
+  @Input() ariaLabel: string;
+  @Input() href: string;
+  @Input() routerLink: string;
+  @Input() target: string;
+  @Input() click: Function;
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
---
+++ src/app/modules/component-library/library/action/overview/overview.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.scss']
+})
+export class OverviewComponent implements OnInit {
+  text: string = 'Action Text';
+  element: string = 'button'; // button, a
+  classes: string = 'btn-primary';
+  ariaLabel: string = '';
+  href: string = 'www.example.com';
+  routerLink: string = 'usage';
+  target: string = '';
+  click = (e) => console.log(e);
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  handleSearch(e) { }
+
+  handleClearSearch(e) { }
+}
