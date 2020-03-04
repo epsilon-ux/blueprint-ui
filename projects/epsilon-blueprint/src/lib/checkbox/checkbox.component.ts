3bcafabadf51b63c5828c41e899677117bf67af2
Kasey Labelle <kasey.labelle@epsilon.com>
2020-03-04 12:34:57 -0500

    wip checkboxes, overview, usage

--- projects/epsilon-blueprint/src/lib/checkbox/checkbox.component.ts
+++ projects/epsilon-blueprint/src/lib/checkbox/checkbox.component.ts
@@ -8,13 +8,28 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CheckboxComponent implements OnInit {
 
   @Input() id: string;
-  @Input() name: string;
-  @Input() value: string;
+  @Input() name?: string;
+  @Input() value?: string;
+  @Input() isChecked = false;
+  @Input() isDisabled = false;
+  @Input() isRequired = false;
+
   @Input() label: string;
+  @Input() visuallyHiddenLabel?: boolean;
+
+  model = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.validation();
   }
 
+  validation() {
+    if (!this.label) {
+      let err = new Error('\'label\' is a required Input of bp-checkbox');
+      err.name = 'Missing Input';
+      throw err;
+    }
+  }
 }
