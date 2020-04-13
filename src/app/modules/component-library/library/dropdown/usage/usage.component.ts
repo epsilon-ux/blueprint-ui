938ad74805289edafb55cfc348a6c07f3df53be9
Ash Johns <ashleypjohns@gmail.com>
2020-04-13 14:45:05 -0400

    fix dropdown usage example

--- src/app/modules/component-library/library/dropdown/usage/usage.component.ts
+++ src/app/modules/component-library/library/dropdown/usage/usage.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsageComponent implements OnInit {
 
+  exampleItems = [
+    {
+      text: 'Dropdown Item',
+      action: 'item1'
+    },
+    {
+      text: 'Dropdown Item',
+      action: 'item2',
+      isDisabled: true
+    },
+    {
+      text: 'Dropdown Item',
+      action: 'item3',
+      isDestructive: true
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
