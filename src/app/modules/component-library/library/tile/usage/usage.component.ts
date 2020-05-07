d45517fd321fd4dd6b56186030b838078d1fb714
Kasey Labelle <kasey.labelle@epsilon.com>
2020-05-07 15:31:06 -0400

    tile docs

--- src/app/modules/component-library/library/tile/usage/usage.component.ts
+++ src/app/modules/component-library/library/tile/usage/usage.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-usage',
   templateUrl: './usage.component.html',
-  styleUrls: ['./usage.component.scss']
+  styleUrls: ['../tile.component.scss']
 })
 export class UsageComponent implements OnInit {
 
