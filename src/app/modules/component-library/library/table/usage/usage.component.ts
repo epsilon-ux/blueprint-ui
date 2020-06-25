c2b4032156f53a6cc4d325f3d7aa32215f8d0ec1
Ash Johns <ashleypjohns@gmail.com>
2020-06-25 13:48:43 -0400

    Update usage docs with some examples

--- src/app/modules/component-library/library/table/usage/usage.component.ts
+++ src/app/modules/component-library/library/table/usage/usage.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Properties, ColumnType } from 'epsilon-blueprint/models/table-models';
+
+import usageData from './tableUsageData';
+import standard from './example-properties/standard';
 
 @Component({
   selector: 'app-usage',
@@ -7,9 +11,170 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsageComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('templateColumn', {static: true}) templateColumn;
+  @ViewChild('expandableRowsTemplate', {static: true}) expandableRowsTemplate;
 
-  ngOnInit() {
+  filteredData = [];
+  tableData = [];
+  pageIndices;
+  isDataLoading = true;
+
+  usageData = usageData;
+  usageDataShort = usageData.slice(0, 2);
+
+  // Properties
+  standard = standard;
+  withCheckboxes = {
+    ...standard,
+    hasSelectableRows: true
+  };
+  withActions = {
+    ...standard,
+    actions: [
+      {
+        element: 'button',
+        text: 'Action',
+        ariaLabel: 'action1 for #{id}',
+        class: 'btn-outline-primary',
+        action: 'action1'
+      },
+      {
+        element: 'button',
+        text: 'Action',
+        ariaLabel: 'action2 for #{id}',
+        class: 'btn-outline-primary',
+        action: 'action2'
+      }
+    ]
+  };
+  withDropdown = {
+    ...standard,
+    actions: [
+      {
+        element: 'button',
+        text: 'Action',
+        ariaLabel: 'action1 for #{id}',
+        class: 'btn-outline-primary',
+        action: 'action1'
+      },
+      {
+        element: 'button',
+        text: 'Action',
+        ariaLabel: 'action1 for #{id}',
+        class: 'btn-outline-primary',
+        action: 'action1'
+      },
+      {
+        element: 'button',
+        text: 'Action',
+        ariaLabel: 'action3 for #{id}',
+        class: 'btn-outline-primary',
+        action: 'action3'
+      }
+    ]
+  };
+  withIcons = {
+    ...standard,
+    columns: [
+      ...standard.columns,
+      {
+        key: 'icon',
+        headerText: 'Icon Column',
+        isColumnDisplayed: true,
+        type: ColumnType.ICON,
+        icon: {
+          color: 'midnight',
+          mapping: {
+            true: 'fa-check',
+            false: ''
+          }
+        }
+      },
+    ]
+  };
+  withStatus = {
+    ...standard,
+    columns: [
+      ...standard.columns,
+      {
+        key: 'status',
+        headerText: 'Status Column',
+        isColumnDisplayed: true,
+        type: ColumnType.STATUS,
+        statusIndicatorMapping: {
+          'incomplete-primary': 'Incomplete Primary',
+          'incomplete-secondary': 'Incomplete Secondary',
+          'in-progress': 'In Progress',
+          'warning': 'Warning',
+          'complete': 'Complete'
+        }
+      },
+    ]
+  };
+  withExpandable = {
+    ...standard,
+    expandableRowsTemplate: this.expandableRowsTemplate
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  handleAction(action: { action: string; id: string }) {
+    // Use action from action buttons to trigger different events here
+  }
+
+  sortByKeyAsc(array, key) {
+    if (key === 'templateCol') {
+      return array.sort((a, b) => a.id < b.id ? -1 : 1);
+    } else {
+      return array.sort((a, b) => {
+        const x = a[key];
+        const y = b[key];
+        return x < y ? -1 : 1;
+      });
+    }
+  }
+
+  sortByKeyDesc(array, key) {
+    if (key === 'templateCol') {
+      return array.sort((a, b) => b.id < a.id ? -1 : 1);
+    } else {
+      return array.sort((a, b) => {
+        const x = a[key];
+        const y = b[key];
+        return x > y ? -1 : 1;
+      });
+    }
+  }
+  
+  handleSort(sort, hasPagination) {
+    if (sort.order === 'ascending') {
+      this.sortByKeyAsc(this.filteredData, sort.column);
+    } else {
+      this.sortByKeyDesc(this.filteredData, sort.column);
+    }
+    if (hasPagination) {
+        this.tableData = this.filteredData.slice(
+        this.pageIndices.start,
+        this.pageIndices.end
+      );
+    }
+  }
+
+  handleSelectedRows(selectedRowIds: {
+    areAllSelected: boolean;
+    selected: {[key: string]: any;}
+  }) {
+    // Handle the selected rows here
+  }
+
+  handlePageChange(pageData) {
+    this.pageIndices = {...pageData.indices};
+    this.tableData = this.filteredData.slice(
+      pageData.indices.start,
+      pageData.indices.end
+    );
   }
 
 }
