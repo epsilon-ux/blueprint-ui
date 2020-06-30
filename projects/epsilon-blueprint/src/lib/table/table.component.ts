3921164a551686c8fc4e7910956cd75d8aebb124
Ash Johns <ashleypjohns@gmail.com>
2020-06-30 17:11:43 -0400

    improve intl and accessibility of table

--- projects/epsilon-blueprint/src/lib/table/table.component.ts
+++ projects/epsilon-blueprint/src/lib/table/table.component.ts
@@ -27,39 +27,6 @@ export class TableComponent implements OnInit, OnChanges {
   @Output() onSort = new EventEmitter();
   @Output() rowSelected = new EventEmitter();
 
-  // Defaults
-  propertyDefaults = {
-    sort: {
-      defaultSortOrder: 'ascending'
-    },
-    hasSelectableRows: false,
-    hasColumnSelector: true,
-    hasDisplayDensity: true,
-    internationalization: {
-      'Select all rows': 'Select all rows',
-      'Actions': 'Actions',
-      'Expand/Collapse': 'Expand/Collapse',
-      'Loading data': 'Loading data',
-      'No data': 'No data available',
-      'Select Row': 'Select Row',
-      'Actions Menu': 'Actions Menu',
-      'Column Selector': 'Column Selector:',
-      'Default': '(Default)',
-      'Showing numVisible out of numTotal':
-        'Showing #{numVisible} out of #{numTotal}',
-      'Display Density': 'Display Density:',
-      'Display Density Options': {
-        'Comfortable': 'Comfortable',
-        'Compact': 'Compact'
-      }
-    }
-  };
-
-  columnDefaults = {
-    isColumnDisplayed: true,
-    isSortable: true
-  };
-
   // Data
   tableData = [];
 
@@ -91,13 +58,46 @@ export class TableComponent implements OnInit, OnChanges {
   }
 
   ngOnInit() {
+    // Defaults
+  const propertyDefaults = {
+    sort: {
+      defaultSortOrder: 'ascending'
+    },
+    hasSelectableRows: false,
+    hasColumnSelector: true,
+    hasDisplayDensity: true,
+    internationalization: {
+      'Select all rows': 'Select all rows',
+      'Actions': 'Actions',
+      'Expand/Collapse': 'Expand/Collapse',
+      'Loading data': 'Loading data',
+      'No data': 'No data available',
+      'Select Row': `Select Row #{${this.properties.rowId}}`,
+      'Toggle Row': `Toggle Row #{${this.properties.rowId}}`,
+      'Actions Menu': 'Actions Menu',
+      'Column Selector': 'Column Selector:',
+      'Default': '(Default)',
+      'Showing numVisible out of numTotal':
+        'Showing #{numVisible} out of #{numTotal}',
+      'Display Density': 'Display Density:',
+      'Display Density Options': {
+        'Comfortable': 'Comfortable',
+        'Compact': 'Compact'
+      }
+    }
+  };
+
+  const columnDefaults = {
+    isColumnDisplayed: true,
+    isSortable: true
+  };
     // Set defaults
     this.properties = Object.assign(
-      this.propertyDefaults,
+      propertyDefaults,
       this.properties
     );
     this.properties.columns = this.properties.columns.map(column =>
-      Object.assign({ ...this.columnDefaults }, column)
+      Object.assign({ ...columnDefaults }, column)
     );
 
     this.sortOrder = this.properties.sort.defaultSortOrder;
