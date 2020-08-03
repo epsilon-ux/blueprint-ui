0ba581ef05f740e1c44ae7a3f505ca11d98df994
Ash Johns <ashleypjohns@gmail.com>
2020-08-03 16:29:59 -0400

    update table model

--- projects/epsilon-blueprint/src/models/table-models.ts
+++ projects/epsilon-blueprint/src/models/table-models.ts
@@ -3,28 +3,11 @@ import { TemplateRef } from '@angular/core';
 export interface Properties {
   caption: string;
   rowId: string;
-  internationalization?: {
-    'Select all rows': string;
-    'Actions': string;
-    'Expand/Collapse': string;
-    'Loading data': string;
-    'No data': string;
-    'Select Row': string;
-    'Actions Menu': string;
-    'Column Selector': string;
-    'Default': string;
-    'Showing numVisible out of numTotal': string;
-    'Display Density': string;
-    'Display Density Options': {
-      'Comfortable': string;
-      'Compact': string;
-    };
-  };
+  internationalization?: Partial<TableIntl>;
   columns: Column[];
   sort: {
     defaultSortedColumn: string;
     defaultSortOrder: string;
-    // TBD: Server Side / Client Side / Restricted
   };
   expandableRowsTemplate?: TemplateRef<any>;
   hasSelectableRows?: boolean;
@@ -32,6 +15,24 @@ export interface Properties {
   hasViewSelector?: boolean;
   hasColumnSelector?: boolean;
   hasDisplayDensity?: boolean;
+}
+
+export interface TableIntl {
+  'Select all rows': string;
+  'Actions': string;
+  'Expand/Collapse': string;
+  'Loading data': string;
+  'No data': string;
+  'Select Row': string;
+  'Actions Menu': string;
+  'Column Selector': string;
+  'Default': string;
+  'Showing numVisible out of numTotal': string;
+  'Display Density': string;
+  'Display Density Options': {
+    'Comfortable': string;
+    'Compact': string;
+  };
 }
 
 export interface Column {
@@ -57,11 +58,11 @@ export interface Column {
     href?: string; // Required if element = 'a' and you need to use an external link
   };
   statusIndicatorMapping?: {
-    'incomplete-primary': string;
-    'incomplete-secondary': string;
-    'in-progress': string;
-    'warning': string;
-    'complete': string;
+    'incomplete-primary'?: string;
+    'incomplete-secondary'?: string;
+    'in-progress'?: string;
+    'warning'?: string;
+    'complete'?: string;
   };
   columnIndex?: number; // This is set internally in the component and should not be provided by the user
 }
@@ -74,15 +75,15 @@ export enum ColumnType {
 }
 
 export interface Action {
-  element: string; // Expect 'a', or 'button'
+  element?: string; // Deprecated: this is no longer needed
   text: string;
   ariaLabel: string; // Can use #{key} syntax to insert values from the row corresponding to the given key
   class: string;
   target?: string; // The target of the link i.e. "_blank" to open in a new tab
   routerLink?: string; // Deprecated: Use bpRouterLink instead
-  bpRouterLink?: string; // Required if element = 'a'
-  href?: string; // Required if element = 'a'
-  action?: string; // Required if element = 'button'
+  bpRouterLink?: string;
+  href?: string;
+  action?: string;
   conditions?: ({
     column: string;
     operator: string;
