2c0f261201ae4743bc52a5ed5d81a4feafb24a48
Ash Johns <ashleypjohns@gmail.com>
2020-08-10 17:15:46 -0400

    clean up breadcrumbs

--- projects/epsilon-blueprint/src/lib/breadcrumbs/breadcrumbs.component.ts
+++ projects/epsilon-blueprint/src/lib/breadcrumbs/breadcrumbs.component.ts
@@ -24,14 +24,21 @@ export class BreadcrumbsComponent implements OnInit {
       throw err;
     }
     this.crumbs.forEach((crumb, i) => {
+      if (!crumb.text) {
+        const err = new Error('Text is a required property of bp-breadcrumbs \'crumbs\' Input');
+        err.name = 'Missing Input';
+        throw err;
+      }
       if (i !== this.crumbs.length - 1) {
         if (!crumb.bpRouterLink && !crumb.href) {
+          // eslint-disable-next-line
           const err = new Error('Must provide either \'bpRouterLink\' or \'href\' for each crumb of bp-breadcrumbs \'crumbs\' Input, except in the last crumb.');
           err.name = 'Missing Input';
           throw err;
         }
       } else {
         if (crumb.bpRouterLink || crumb.href || crumb.target) {
+          // eslint-disable-next-line
           const err = new Error('The last crumb of the \'crumbs\' Input in bp-breadcrumbs can only have a text property.');
           err.name = 'Invalid Input';
           throw err;
