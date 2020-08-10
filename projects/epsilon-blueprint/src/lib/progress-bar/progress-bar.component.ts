eb1f29f3fafb7ca14c0e719399cdec716df00c56
Kasey Labelle <kasey.labelle@epsilon.com>
2020-08-10 17:00:40 -0400

    add todos and change input names

--- projects/epsilon-blueprint/src/lib/progress-bar/progress-bar.component.ts
+++ projects/epsilon-blueprint/src/lib/progress-bar/progress-bar.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class ProgressBarComponent implements OnInit {
 
-  @Input() ariaValueMin: string;
-  @Input() ariaValueMax: string;
-  @Input() ariaValueNow: string;
+  @Input() min: string;
+  @Input() max: string;
+  @Input() value: string;
   @Input() classes = 'progress';
   @Input() hasProgressText?: boolean;
 
