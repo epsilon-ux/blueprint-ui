b06bbe437341346fde7af319c08889c62f40ff51
Ash Johns <ashleypjohns@gmail.com>
2020-09-14 15:52:36 -0400

    Hook up header

--- projects/epsilon-blueprint/src/lib/header/header.component.ts
+++ projects/epsilon-blueprint/src/lib/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { generateUniqueId, isDefined } from '../../helpers';
 
 @Component({
   selector: 'bp-header',
@@ -8,12 +9,34 @@ export class HeaderComponent implements OnInit {
 
   @Input() logoSrc: string;
   @Input() logoAlt: string;
+  @Input() logoHref?: string;
+  @Input() logoRouterLink?: string;
   @Input() hasCollapsibleMenu?: boolean = true;
   @Input() hasContextSelector?: boolean = true;
   @Input() hasNavigation?: boolean = true;
+  @Input() expandBreakpoint?: 'sm' | 'md' | 'lg' | 'xl' = 'lg';
+  @Input() bpID: string;
+  @Input() internationalization: {
+    'Menu': string;
+    'MenuAriaLabel': string;
+  };
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    if (!this.bpID) {
+      this.bpID = 'collapsibleMenu' + String(generateUniqueId());
+    }
+
+    const intlDefaults = {
+      'Menu': 'Menu',
+      'MenuAriaLabel': 'Toggle primary navigation'
+    };
+    Object.assign(intlDefaults, this.internationalization);
+  }
+
+  isLogoLink(): boolean {
+    return isDefined(this.logoHref) || isDefined(this.logoRouterLink);
+  }
 
 }
