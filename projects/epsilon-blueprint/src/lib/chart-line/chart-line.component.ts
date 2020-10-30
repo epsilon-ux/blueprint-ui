12edb381dea32bfd5428b8f8a94964e786075c86
Ash Johns <ashleypjohns@gmail.com>
2020-10-30 16:58:41 -0400

    Clean up selections

--- projects/epsilon-blueprint/src/lib/chart-line/chart-line.component.ts
+++ projects/epsilon-blueprint/src/lib/chart-line/chart-line.component.ts
@@ -49,14 +49,19 @@ export class ChartLineComponent implements OnInit, AfterViewInit, OnChanges {
 
   svg: any;
   color: d3.ScaleOrdinal<string, unknown>;
-  xScale: any;
-  yScale: any;
+  xScale: d3.ScaleTime<number, number>;
+  yScale: d3.ScaleLinear<number, number>;
   xAxis: any;
   yAxis: any;
+
+  renderedXAxis;
+  renderedYAxis;
 
   groups;
 
   resizeListener;
+  renderedLines: any;
+  line: d3.Line<[number, number]>;
 
   constructor() { }
 
@@ -72,7 +77,10 @@ export class ChartLineComponent implements OnInit, AfterViewInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.data?.currentValue && !changes.data?.firstChange) {
       this.prepare();
-      this.update();
+      this.updateAxes();
+      this.updateLines();
+      this.updatePoints();
+      rescale(this.bpID, this.width);
     }
   }
 
@@ -96,7 +104,23 @@ export class ChartLineComponent implements OnInit, AfterViewInit, OnChanges {
         / 2})`
       );
 
-    this.update();
+    this.renderedLines = this.svg
+      .append('g')
+      .attr('fill', 'none')
+      .attr('stroke-width', 2);
+
+    this.renderedXAxis = this.svg
+      .append('g')
+      .attr('transform', `translate(0, ${this.height + this.margin.top})`);
+
+    this.renderedYAxis = this.svg
+      .append('g')
+      .attr('transform', `translate(${this.margin.left}, 0)`);
+
+    this.updateAxes();
+    this.updatePoints();
+    this.updateLines();
+    this.createHoverZones();
     setTimeout(() => {
       rescale(this.bpID, this.width);
     }, 0);
@@ -109,9 +133,7 @@ export class ChartLineComponent implements OnInit, AfterViewInit, OnChanges {
       .scaleOrdinal()
       .domain(groups)
       .range(qualitativeColors.slice(0, groups.length));
-  }
 
-  update(): void {
     const xValues = Array.from(this.data, d => d[this.xValue]);
     const yValues = Array.from(this.data, d => d[this.yValue]);
     const yExtent: [number, number] = d3.extent(yValues);
@@ -132,12 +154,11 @@ export class ChartLineComponent implements OnInit, AfterViewInit, OnChanges {
       .range([this.height + this.margin.top, this.margin.top])
       .nice();
 
-    this.createAxes();
-    this.createPoints();
-    this.createLines();
-  }
+    this.line = d3.line()
+      // .defined(d => !isNaN(d))
+      .x(d => this.xScale(d[this.xValue]))
+      .y(d => this.yScale(d[this.yValue]));
 
-  createAxes(): void {
     this.xAxis = d3
       .axisBottom(this.xScale)
       .ticks(5)
@@ -153,51 +174,101 @@ export class ChartLineComponent implements OnInit, AfterViewInit, OnChanges {
       .tickSize(0)
       .tickPadding(10)
       .tickFormat(this.formatters.yAxis);
+  }
 
-    this.svg
-      .append('g')
-      .attr('transform', `translate(0, ${this.height + this.margin.top})`)
+  updateAxes(): void {
+    this.renderedXAxis
+      .transition()
+      .ease(d3.easeExpOut)
+      .duration(400)
       .call(this.xAxis);
 
-    this.svg
-      .append('g')
-      .attr('transform', `translate(${this.margin.left}, 0)`)
+    this.renderedYAxis
+      .transition()
+      .ease(d3.easeExpOut)
+      .duration(400)
       .call(this.yAxis);
   }
 
-  createPoints(): void {
-    this.groups.forEach(group => {
-      const fillColor = this.color(group[0]);
-      this.svg
-        .append('g')
-        .selectAll('circle')
-        .data(group[1])
-        .join('circle')
-        .attr('class', 'point')
-        .attr('fill', d => fillColor)
-        .attr('stroke', d => fillColor)
-        .attr('cx', d => this.xScale(d[this.xValue]))
-        .attr('cy', d => this.yScale(d[this.yValue]))
-        .attr('r', 5);
-    });
+  updatePoints(): void {
+    const _this = this;
+    this.svg
+      .selectAll('.point-group')
+      .data(this.groups)
+      .join('g')
+      .attr('class', 'point-group')
+      .attr('fill', d => this.color(d[0]))
+      .attr('stroke', d => this.color(d[0]))
+      .each(function(d) {
+        const points = d3.select(this)
+          .selectAll('circle')
+          .data(d[1]);
+
+        points
+          .enter()
+          .append('circle')
+          .attr('class', 'point')
+          .attr('r', 5)
+          .attr('cx', d => _this.xScale(d[_this.xValue]))
+          .attr('cy', 0)
+          .transition()
+          .ease(d3.easeExpOut)
+          .duration(400)
+          .attr('cy', d => _this.yScale(d[_this.yValue]));
+
+        points
+          .transition()
+          .ease(d3.easeExpOut)
+          .duration(400)
+          .attr('cx', d => _this.xScale(d[_this.xValue]))
+          .attr('cy', d => _this.yScale(d[_this.yValue]));
+
+        points
+          .exit()
+          .transition()
+          .ease(d3.easeExpOut)
+          .duration(400)
+          .attr('cy', 0)
+          .remove();
+      });
   }
 
-  createLines(): void {
-    const line = d3.line()
-      //.defined(d => !isNaN(d))
-      .x(d => this.xScale(d[this.xValue]))
-      .y(d => this.yScale(d[this.yValue]));
-
-    this.svg
-      .append('g')
-      .attr('fill', 'none')
-      .attr('stroke-width', 2)
-      .selectAll('path')
+  updateLines(): void {
+    this.renderedLines
+      .selectAll('.line')
       .data(this.groups)
       .join('path')
       .attr('class', 'line')
       .attr('stroke', d => this.color(d))
-      .attr('d', d => line(d[1]));
+      .transition()
+      .ease(d3.easeExpOut)
+      .duration(400)
+      .attr('d', d => this.line(d[1]));
+  }
+
+  createHoverZones(): void {
+    const hoverRegionWidth = this.width / this.getXLength();
+    this.groups.forEach(group => {
+      this.svg
+        .append('g')
+        .selectAll('rect')
+        .data(group[1])
+        .join('rect')
+        .attr('class', 'chart-hover-zone')
+        .attr('fill', 'rgba(0, 0, 0, 0)')
+        .attr('x', d => this.xScale(d[this.xValue]) - hoverRegionWidth / 2)
+        .attr('width', hoverRegionWidth)
+        .attr('height', this.height + this.margin.top)
+        .on('mouseover', (e, d) => {
+          // TODO: Implement Tooltip
+        });
+    });
+  }
+
+  getXLength(): number {
+    return this.groups.reduce((accumulator: number, current: [string, []]) => {
+      return current[1].length > accumulator ? current[1].length : accumulator;
+    }, 0) as number;
   }
 
 }
